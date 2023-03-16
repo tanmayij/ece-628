3b05c945590ffbee1b9d8a0f3231ff4a8e005db0
Shwetha Saravanan <70532331+shwetha-saravanan@users.noreply.github.com>
2023-03-16 15:47:46 -0400

    Update merkle.py

--- merkle.py
+++ merkle.py
@@ -16,8 +16,7 @@ random_numbers = [prng(bytes(str(i + 900), 'utf-8')) for i in range(n)]
 file1 = open("data.txt", "r")
 for i in file1:
     data = str(i)
-#d = str(open("data.txt", "r")) #this should actually be a text file of 1000 bits.
-#print ("printing file:",data)
+
 leaves = [h(str(i + 900) + data + str(random_numbers[i])) for i in range(n)]
 class MerkleTreeNode:
     def __init__(self,value):
@@ -42,8 +41,8 @@ def buildTree(leaves,f):
                 temp.append(nodes[i])
                 break
             #print ("node a[", i, "][",i+1,"]")#hash value: ", node1.hashValue)
-            f.write("Left child : "+ str(node1.value) + " | Hash : " + str(node1.hashValue) + " \n")
-            f.write("Right child : "+ str(node2.value) + " | Hash : " + str(node2.hashValue) +" \n")
+            #f.write("Left child : "+ str(node1.value) + " | Hash : " + str(node1.hashValue) + " \n")
+            #f.write("Right child : "+ str(node2.value) + " | Hash : " + str(node2.hashValue) +" \n")
             concatenatedHash = str(node1.hashValue) + str(node2.hashValue)
             parent = MerkleTreeNode(concatenatedHash)
             parent.left = node1
@@ -53,9 +52,31 @@ def buildTree(leaves,f):
         nodes = temp 
     return nodes[0]
 
+
+def retrieve_and_verify(data, root, leaves):
+    # Retrieve the leaf node corresponding to the data
+    leaf_index=
+    leaf = leaves[leaf_index]
+
+    # Traverse up the tree from the leaf node to the root node, computing the hash value of each parent node
+    current_node = leaf
+    while current_node != root:
+        parent = current_node.left if current_node.right == None else MerkleTreeNode(str(current_node.left.hashValue) + str(current_node.right.hashValue))
+        current_node = parent
+
+    # Compare the final computed root hash value with the expected root hash value
+    expected_root_hash = root.hashValue
+    actual_root_hash = current_node.hashValue
+    if expected_root_hash == actual_root_hash:
+        print("Data is verified to be part of the Merkle tree.")
+    else:
+        print("Data is not part of the Merkle tree.")
+
 #inputString = sys.argv[1]
 #leaves = data
 #leaves = leavesString.split(",")
 f = open("merkle.tree", "w")
 root = buildTree(leaves,f)
+retrieve_and_verify(data, root, leaves)
+
 f.close()
